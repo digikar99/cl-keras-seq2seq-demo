3f53897e53e7c1927fbe99a34c762948685ca0d8
digikar99 <shubhamkarayare@gmail.com>
2020-03-10 23:03:18 +0530

    fixed to work with py4cl2 v2.2.1

--- seq2seq.lisp
+++ seq2seq.lisp
@@ -1,20 +1,14 @@
-;; Local Variables:
-;; eval: (put 'defparameters 'lisp-indent-function 0)
-;; eval: (put 'dlet* 'lisp-indent-function 1)
-;; End:
-
-(py4cl:defpymodule "numpy" nil :lisp-package "NP")
-(py4cl:defpymodule "pickle")
-(py4cl:defpymodule "keras.layers" nil)
-(py4cL:defpymodule "keras" nil)
+(eval-when (:compile-toplevel :load-toplevel)
+  (py4cl2:defpymodule "numpy")
+  (py4cl2:defpymodule "pickle")
+  (py4cl2:defpymodule "keras" t))
 
 (defpackage :seq2seq
-  (:use :cl :digikar-utilities :iterate :py4cl)
-  (:shadowing-import-from :keras.layers :dense/class :lstm/class :input)
+  (:use :cl :reader :iterate :py4cl2)
+  (:shadowing-import-from :keras.layers :dense/class :lstm/class :input/1)
   (:shadowing-import-from :keras.models :load-model)
   (:shadowing-import-from :keras :model/class)
   (:shadowing-import-from :cl-utilities :split-sequence))
-
 
 (in-package :seq2seq)
 
@@ -39,26 +33,30 @@
       `(progn ,@body)))
 
 (defparameters
-  (*batch-size* 2 "Batch size for training.")
-  (*epochs* 100 "Batch size for Number of epochs to train for.")
-  (*latent-dim* 256 "Latent dimensionality of the encoding space.")
-  (*num-samples* 10 "Number of samples to train on.")
-  (*data-path* "fra.txt")
+    (*batch-size* 32 "Batch size for training.")
+    (*epochs* 40 "Batch size for Number of epochs to train for.")
+  (*latent-dim* 512 "Latent dimensionality of the encoding space.")
+  (*num-samples* 1000 "Number of samples to train on.")
+  (*data-path* "DATASET.txt")
+  ;; (*data-path* "fra.txt")
   (*validation-split* 0.2))
 
 (defun define-compiled-model (num-encoder-tokens num-decoder-tokens latent-dim)
 
-  (let* ((encoder-inputs (input :shape `(nil ,num-encoder-tokens)))
+  (let* ((encoder-inputs (input/1 :shape `("None" ,num-encoder-tokens)))
          (encoder (lstm :units latent-dim
                         :return-state t))
+         (encoder-lstm (keras.layers:bidirectional/class
+                        :layer (lstm :units latent-dim
+                                     :return-state t)))
          (encoder-output-with-states (pycall encoder encoder-inputs))
          ;; (encoder-outputs (pyeval encoder-output-with-states "[0]"))
          (encoder-states (pyeval encoder-output-with-states "[1:]")) 
 
-         (decoder-inputs (input :shape `(nil ,num-decoder-tokens)))
-         (decoder-lstm (print (lstm :units latent-dim
+         (decoder-inputs (input/1 :shape `("None" ,num-decoder-tokens)))
+         (decoder-lstm (lstm :units latent-dim
                              :return-state t
-                             :return-sequences t)))
+                             :return-sequences t))
          (decoder-outputs-after-lstm
           (get-val (pycall decoder-lstm
                            decoder-inputs
@@ -77,15 +75,15 @@
                     &key (batch-size *batch-size*)
                       (epochs *epochs*)
                       (validation-split *validation-split*))
-  (pymethod-monitor model 'fit
-                    (list (vector encoder-input-data decoder-input-data)
-                          decoder-target-data
-                          :batch-size batch-size
-                          :epochs epochs
-                          :validation-split validation-split)))
+  (pymethod model 'fit
+            (vector encoder-input-data decoder-input-data)
+            decoder-target-data
+            :batch-size batch-size
+            :epochs epochs
+            :validation-split validation-split))
 
-(let ((input-token-index-table (make-hash-table :size 100))    ; sets
-      (target-token-index-table (make-hash-table :size 100))
+(let ((input-token-index-table (make-hash-table))    ; sets
+      (target-token-index-table (make-hash-table))
       (input-token-list nil)
       (target-token-list nil)
       (max-encoder-seq-length nil)
@@ -105,7 +103,7 @@ target-texts"
 
           (collect input-text into input-texts)
           (collect target-text into target-texts)
-          (finally (return (list i input-texts target-texts)))))
+          (finally (format t "I: ~D~%" i) (return (list i input-texts target-texts)))))
 
   (defun process-texts-to-characters (input-texts target-texts)
     "Populates token-index-table and token-list."
@@ -113,8 +111,8 @@ target-texts"
           (for input-text in input-texts)
           (generate j from 0)
           (iter (for input-char in-string input-text)
-                (unless (get-val input-token-index-table input-char 'hash-table)
-                  (setf (get-val input-token-index-table input-char 'hash-table)
+                (unless (get-val input-token-index-table input-char)
+                  (setf (get-val input-token-index-table input-char)
                         (in outer (next j)))
                   (setq input-token-list (cons input-char input-token-list))))
           (finally (setq input-token-list (reverse input-token-list ))
@@ -124,8 +122,8 @@ target-texts"
           (for target-text in target-texts)
           (generate j from 0)
           (iter (for target-char in-string target-text)
-                (unless (get-val target-token-index-table target-char 'hash-table)
-                  (setf (get-val target-token-index-table target-char 'hash-table)
+                (unless (get-val target-token-index-table target-char)
+                  (setf (get-val target-token-index-table target-char)
                         (in outer (next j)))
                   (setq target-token-list (cons target-char target-token-list))))
           (finally (setq target-token-list (reverse target-token-list))
@@ -155,7 +153,7 @@ target-texts"
                            :element-type 'single-float)))
       (iter (for char in-string input-text)
             (for i upfrom 0)
-            (setf (get-val arr (list 0 i (get-val input-token-index-table char)))
+            (setf (get-val arr 0 i (get-val input-token-index-table char))
                   1.0))
       arr))
   (defun target-text-to-3d-array (target-text)
@@ -165,7 +163,7 @@ target-texts"
                            :element-type 'single-float)))
       (iter (for char in-string target-text)
             (for i upfrom 0)
-            (setf (get-val arr (list 0 i (get-val target-token-index-table char)))
+            (setf (get-val arr 0 i (get-val target-token-index-table char))
                   1.0))
       arr))
   
@@ -194,35 +192,35 @@ target-texts"
               (iter (for id from 0)
                     (for input-char in-string input-text)
                     (setf (get-val encoder-input-data
-                                   (list j id (get-val input-token-index-table
-                                                       input-char)))
+                                   j id (get-val input-token-index-table
+                                                 input-char))
                           1.0))
               (iter (for id from 0)
                     (for target-char in-string target-text)
                     (setf (get-val decoder-input-data
-                                   (list j id (get-val target-token-index-table
-                                                       target-char)))
+                                   j id (get-val target-token-index-table
+                                                 target-char))
                           1.0)
                     (unless (first-iteration-p)
                       (setf (get-val decoder-target-data
-                                     (list j (1- id) (get-val target-token-index-table
-                                                              target-char)))
+                                     j (1- id) (get-val target-token-index-table
+                                                        target-char))
                             1.0))))
         (list encoder-input-data decoder-input-data decoder-target-data)))))
 
 (defun restore-trained-model-for-inference
     (weights-file num-encoder-tokens num-decoder-tokens latent-dim)
   "Returns a two-item list corresponding to encoder and decoder models."
-  (let* ((encoder-inputs (input :shape `(nil ,num-encoder-tokens)))
+  (let* ((encoder-inputs (input/1 :shape `(nil ,num-encoder-tokens)))
          (encoder (lstm :units latent-dim
                         :return-state t))
          (encoder-output-with-states (pycall encoder encoder-inputs))
          (encoder-states (pyeval encoder-output-with-states "[1:]")) 
 
-         (decoder-inputs (input :shape `(nil ,num-decoder-tokens)))
+         (decoder-inputs (input/1 :shape `(nil ,num-decoder-tokens)))
          (decoder-lstm (print (lstm :units latent-dim
-                             :return-state t
-                             :return-sequences t)))
+                                    :return-state t
+                                    :return-sequences t)))
          (decoder-outputs-after-lstm
           (get-val (pycall decoder-lstm
                            decoder-inputs
@@ -237,8 +235,8 @@ target-texts"
     (pymethod model 'compile :optimizer "rmsprop" :loss "categorical_crossentropy")
     
     (list (model encoder-inputs encoder-states)
-          (let* ((decoder-c (input :shape `(,latent-dim)))
-                 (decoder-h (input :shape `(,latent-dim)))
+          (let* ((decoder-c (input/1 :shape `(,latent-dim)))
+                 (decoder-h (input/1 :shape `(,latent-dim)))
                  (decoder-states-input (vector decoder-c decoder-h))
                  (decoder-output-with-states
                   (pycall decoder-lstm
@@ -252,16 +250,16 @@ target-texts"
 
 
 (defun define-encoder-model (trained-model num-encoder-tokens)
-  (let* ((encoder-inputs (input :shape `(nil ,num-encoder-tokens)))
+  (let* ((encoder-inputs (input/1 :shape `("None" ,num-encoder-tokens)))
          (encoder (get-val (pyslot-value trained-model 'layers) 2))
          (encoder-output-with-states (pycall encoder encoder-inputs))
          (encoder-states (pyeval encoder-output-with-states "[1:]")))
     (model encoder-inputs encoder-states)))
 
 (defun define-decoder-model (trained-model num-decoder-tokens latent-dim)
-  (let* ((decoder-inputs (input :shape `(nil ,num-decoder-tokens)))
-         (decoder-h (input :shape `(,latent-dim)))
-         (decoder-c (input :shape `(,latent-dim)))
+  (let* ((decoder-inputs (input/1 :shape `("None" ,num-decoder-tokens)))
+         (decoder-h (input/1 :shape `(,latent-dim)))
+         (decoder-c (input/1 :shape `(,latent-dim)))
          (decoder-states-input (vector decoder-h decoder-c))
          (decoder-lstm (get-val (pyslot-value trained-model 'layers) 3))
          (decoder-output-with-states
@@ -272,7 +270,7 @@ target-texts"
          (decoder-dense (get-val (pyslot-value trained-model 'layers) 4))
          (decoder-outputs (pycall decoder-dense
                                   (pyeval decoder-output-with-states "[0]"))))
-    (model (vector decoder-inputs decoder-h decoder-c)
+    (model (pyeval "[" decoder-inputs "] + " decoder-states-input)
            (pyeval "[" decoder-outputs "] +" decoder-states))))
 
 (defun decode-sequence (input-sequence encoder-model decoder-model
@@ -280,7 +278,7 @@ target-texts"
   
   (flet ((set-to-1 (1-index)
            (let ((arr (numpy:zeros `(1 1 ,num-decoder-tokens))))
-             (setf (get-val arr `(0 0 ,1-index)) 1)
+             (setf (get-val arr 0 0 1-index) 1)
              arr)))
     
     (iter (for states-value
